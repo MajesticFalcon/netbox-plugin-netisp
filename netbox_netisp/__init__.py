0c5610dc29e603ec107f4113c915b00c435c627f
Schylar Utley <schylarutley@hotmail.com>
2021-02-09 23:15:16 +0000

    initial build

---
+++ netbox_netisp/__init__.py
@@ -0,0 +1,16 @@
+from extras.plugins import PluginConfig
+
+
+class NetISP(PluginConfig):
+    name = "netbox_netisp"
+    verbose_name = "net isp"
+    description = "Manages isp connections"
+    version = "0.1"
+    author = "sutley"
+    author_email = "sutley5.com"
+    base_url = "sutley"
+    required_settings = []
+    default_settings = {}
+
+
+config = NetISP
