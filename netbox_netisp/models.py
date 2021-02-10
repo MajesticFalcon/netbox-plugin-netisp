e6e171227252624d16f7c67f7e617654adb7bbe4
Schylar Utley <schylarutley@hotmail.com>
2021-02-10 22:27:24 +0000

    add test for derived age

--- netbox_netisp/models.py
+++ netbox_netisp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from extras.models import ChangeLoggedModel
+from datetime import datetime
 
 class Customer(ChangeLoggedModel):
     first_name = models.CharField(max_length=30)
--- netbox_netisp/tests/test_customer.py
+++ netbox_netisp/tests/test_customer.py
@@ -1,16 +1,27 @@
-"""Unit tests for netbox-netisp REST API."""
+"""Unit tests for netbox-netisp ."""
 
 
 from django.test import TestCase
-from models import Customer
+from netbox_netisp.models import Customer
+from datetime import datetime
 
 class CustomerTestCase(TestCase):
     """Test the Customer"""
     def setUp(self):
-        """Create a customer object"""
-        Customer.objects.create(first_name="Schylar", middle_name="S", last_name="Utley")
+        """Create a customer object with sample data"""
+        Customer.objects.create(
+            first_name="Schylar",
+            middle_name="S",
+            last_name="Utley",
+            birthdate=datetime(1997,2,10,16,13,49,71549)
+        )
 
     def test_customer_name(self):
         """Verify that the name is being joined correctly"""
         sutley = Customer.objects.get(first_name="Schylar")
         self.assertEqual(sutley.name(), 'Schylar S Utley')
+
+    def test_customer_age(self):
+        """Verify that the customer has a valid age"""
+        sutley = Customer.objects.get(first_name="Schylar")
+        self.assertGreaterEqual(sutley.age(), 1)
