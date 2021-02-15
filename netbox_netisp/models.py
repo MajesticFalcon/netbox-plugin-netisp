748e136d8c2d3479d81c9e35f41a0fad10c02e6e
Evan Clay <clayevan.clay@gmail.com>
2021-02-15 15:35:00 -0600

    finish issue #4

--- netbox_netisp/models.py
+++ netbox_netisp/models.py
@@ -4,14 +4,20 @@ from datetime import datetime
 from django.urls import reverse
 
 class Customer(ChangeLoggedModel):
-    first_name = models.CharField(max_length=30)
-    middle_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birthdate = models.DateTimeField(blank = True, null = True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=255, blank=True, null=True)
     slug = models.SlugField(unique=True)
+        
 
     def name(self):
         return "{0} {1} {2}".format(self.first_name, self.middle_name, self.last_name)
-
+    def age(self):
+        return datetime.now().year - self.birthday.year
     def get_absolute_url(self):
         return reverse('plugins:netbox_netisp:customer', args=[self.slug])
 
